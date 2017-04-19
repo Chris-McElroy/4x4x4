35a40955e47b9bcf9ac49cfea6647cfe4cc08b76
ChristopherMcElroy <csmcelroy@me.com>
2017-04-19 12:36:32 -0700

    testing more

--- BrainTest.py
+++ BrainTest.py
@@ -8,16 +8,21 @@ class boardTest(unittest.TestCase):
 	onesBoard = [[[1 for i in range(4)] for j in range(4)] for k in range(4)]
 	twosBoard = [[[2 for i in range(4)] for j in range(4)] for k in range(4)]
 	# single 1 at 0,0,0
-	oneMoveBoard = openBoard
+	oneMoveBoard = [[[0 for i in range(4)] for j in range(4)] for k in range(4)]
 	oneMoveBoard[0][0][0] = 1
-	# 1 at 0,0,0, 1 at 3,2,1, 2 at 3,2,1, 2 at 0,1,2. 
-	fourMoveBoard = oneMoveBoard
+	# 1 at 0,0,0, 1 at 3,2,1, 2 at 1,2,3, 2 at 0,1,2. 
+	fourMoveBoard = [[[0 for i in range(4)] for j in range(4)] for k in range(4)]
+	fourMoveBoard[0][0][0] = 1
 	fourMoveBoard[3][2][1] = 1
 	fourMoveBoard[1][2][3] = 2
 	fourMoveBoard[0][1][2] = 2
+	# line boards
+	oneLineXBoard = [[[1 if (i%4 == 0 and j%4 == 0) else 0 for i in range(4)] for j in range(4)] for k in range(4)]
+	oneLineYBoard = [[[1 if (i%4 == 0 and k%4 == 0) else 0 for i in range(4)] for j in range(4)] for k in range(4)]
+	oneLineZBoard = [[[1 if (k%4 == 0 and j%4 == 0) else 0 for i in range(4)] for j in range(4)] for k in range(4)]
 	# plane is z = 0
-	onePlaneFull = [[[1 if (k%4 == 0) 0 else for i in range(4)] for j in range(4)] for k in range(4)]
-	
+	onePlaneBoard = [[[1 if (i%4 == 0) else 0 for i in range(4)] for j in range(4)] for k in range(4)]
+
 
 if __name__ == '__main__':
     unittest.main()
@@ -27,4 +32,3 @@ if __name__ == '__main__':
 
 
 
-		
