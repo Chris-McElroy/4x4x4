a269b0fd88eb1d3882a65916ee0aa01c8f4caa6c
Alex Goldstein <abg.goldstein@gmail.com>
2017-05-19 19:29:04 -0700

    Prep for big wildfire update
    Only changes under the hood so far, nothing behavior-wise changed

--- AIs/Wildfire.py
+++ AIs/Wildfire.py
@@ -9,7 +9,7 @@ class Wildfire:
 	def __init__(self):
 		""" Stores player info for easy access """
 		self.MAX_FORCES = 9
-		self.forceCache = {}
+		self.forceCache = set()
 
 	def move(self,board,n, d):
 		"""
@@ -23,15 +23,15 @@ class Wildfire:
 			return self.winningMove(board, board.otherNumber(n))
 
 		# if we have a winning sequence of forces, take it
-		winningForce = self.forceCheck(board, n, self.MAX_FORCES)
-		if winningForce:
-			return winningForce
+		winningForces = self.forceCheck(board, n, self.MAX_FORCES)
+		if winningForces:
+			return winningForces[0][0]
 
 		# if we will have a losing sequence of forces, we need to take action against it.
-		otherForce = self.forceCheck(board, board.otherNumber(n), self.MAX_FORCES)
+		otherForces = self.forceCheck(board, board.otherNumber(n), self.MAX_FORCES)
 		# currently just do shittiest possible block
-		if otherForce:
-			return otherForce
+		if otherForces:
+			return otherForces[0][0]
 
 
 		bestScore = -999
@@ -54,14 +54,8 @@ class Wildfire:
 		return random.choice(bestMoves)
 
 
-		# placeholder acting like a human until I fill in more.
-		move = False
-		while not move:
-			time.sleep(.1)
-			move = d.getMove()
-		return move
-
 	def winningMove(self, board, n):
+		""" Checks if there is a winning move for player n and if there is, returns it"""
 		forces = board.findLines(n, 3).copy()
 		if len(forces) > 0:
 			for point in board.lineToPoints(next(iter(forces))):
@@ -69,23 +63,31 @@ class Wildfire:
 					return point
 	
 	def forceCheck(self, board, n, movesLeft):
-		self.forceCache = {}
+		""" Wrapper function for forcecheckrec"""
+		self.forceCache.clear()
 		return self.forceCheckRec(board, n, movesLeft)
 
 	def forceCheckRec(self, board, n, movesLeft):
+		""" Recursively finds all possible forcing sequences with {movesleft} moves
+			for player n up to reordering and returns them"""
 
-		if self.forceCache.has_key(board.hash()):
-			return self.forceCache[board.hash()]
+		if board.hash() in self.forceCache:
+			# we've already visited this, no need to do so again
+			return False
 
 		if self.winningMove(board, n):
-			self.forceCache[board.hash()] = self.winningMove(board, n)
-			return self.winningMove(board, n)
+			self.forceCache.add(board.hash())
+			# this looks weird, but we're returning a list of
+			# possible lists of moves
+			return [[self.winningMove(board, n)]]
 
 		if self.winningMove(board, board.otherNumber(n)) or movesLeft == 0:
-			self.forceCache[board.hash()] = 0
-			return 0
+			self.forceCache.add(board.hash())
+			return False
 
 		possibleForces =  board.findLines(n, 2)
+
+		wins = []
 
 		for force in possibleForces:
 			openSpots = filter(lambda p: board.pointToValue(p) == 0, board.lineToPoints(force))
@@ -93,29 +95,30 @@ class Wildfire:
 			board.move(n, openSpots[0])
 			board.move(board.otherNumber(n), openSpots[1])
 
-			success = self.forceCheckRec(board, n, movesLeft-1)
+			wins1 = self.forceCheckRec(board, n, movesLeft-1)
 			board.clearPoint(openSpots[0])
 			board.clearPoint(openSpots[1])
 
 			# we don't have getters or setters because this is sus
 			# kids, don't do this at home
 
-			if success:
-				self.forceCache[board.hash()] = openSpots[1]
-				return openSpots[0]
+			if wins1:
+				self.forceCache.add(board.hash())
+				wins += [openSpots + moves for moves in wins1]
 			
 			board.move(n, openSpots[1])
 			board.move(board.otherNumber(n), openSpots[0])
 
-			success = self.forceCheckRec(board, n, movesLeft-1)
+			wins2 = self.forceCheckRec(board, n, movesLeft-1)
 			board.clearPoint(openSpots[0])
 			board.clearPoint(openSpots[1])
 
-			if success:
-				self.forceCache[board.hash()] = openSpots[1]
-				return openSpots[1]
+			if wins2:
+				self.forceCache.add(board.hash())
+				openSpots.reverse()
+				wins += [openSpots + moves for moves in wins2]
 
-		return 0
+		return wins
 
 	def evaluatePosition(self, board, n):
 		score = 0
@@ -123,10 +126,6 @@ class Wildfire:
 		score += len(board.findLines(n,2))
 		score -= len(board.findLines(board.otherNumber(n),1))
 		score -= len(board.findLines(board.otherNumber(n),2))
-
-
-		if self.forceCheck(board, board.otherNumber(n), self.MAX_FORCES-3) != 0:
-			return 0
 
 		return score
 
